ccaaf6e6b137a741e80cedfef8d3fd90af9853ec
Noelle Leigh <5957867+noelleleigh@users.noreply.github.com>
2025-09-03 10:57:37 -0400

    Add type hints (#12)
    * Add type hints to function arguments
    
    * _escape: Avoid reassigning the type of `payload`
    
    * writer: Add positional argument delimiter
    
    This matches the documentation:
    https://docs.python.org/3/library/csv.html#csv.writer
    
    * Quote _escape() return type
    
    * Replace TypeVar with Any
    
    * _ProxyWriter.writerows: Add return type

--- defusedcsv/csv.py
+++ defusedcsv/csv.py
@@ -1,3 +1,5 @@
+from typing import TYPE_CHECKING, Any
+
 import re
 from csv import (
     QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONE, QUOTE_NONNUMERIC, Dialect,
@@ -8,6 +10,10 @@ from csv import (
 from numbers import Number
 
 from . import version as __version__
+
+if TYPE_CHECKING:
+    import io
+    from collections.abc import Iterable, Sequence
 
 try:
     # Requires Python >= 3.12
@@ -25,24 +31,24 @@ __all__ = ["QUOTE_MINIMAL", "QUOTE_ALL", "QUOTE_NONNUMERIC", "QUOTE_NONE",
            "unix_dialect"]
 
 
-def _escape(payload):
+def _escape(payload: Any) -> "None | Number | str":
     if payload is None:
         return payload
     if isinstance(payload, Number):
         return payload
 
-    payload = str(payload)
-    if payload and payload[0] in ('@', '+', '-', '=', '|', '%') and not re.match("^-?[0-9,\\.]+$", payload):
-        payload = payload.replace("|", "\\|")
-        payload = "'" + payload
-    return payload
+    str_payload = str(payload)
+    if str_payload and str_payload[0] in ('@', '+', '-', '=', '|', '%') and not re.match("^-?[0-9,\\.]+$", str_payload):
+        str_payload = str_payload.replace("|", "\\|")
+        str_payload = "'" + str_payload
+    return str_payload
 
 
 class _ProxyWriter:
     def __init__(self, writer):
         self.writer = writer
 
-    def writerow(self, row):
+    def writerow(self, row: "Iterable[Any]") -> None:
         try:
             iter(row)
         except TypeError as err:
@@ -50,19 +56,19 @@ class _ProxyWriter:
             raise Error(msg) from err
         return self.writer.writerow(_escape(field) for field in row)
 
-    def writerows(self, rows):
+    def writerows(self, rows: "Iterable[Iterable[Any]]") -> None:
         return self.writer.writerows((_escape(field) for field in row) for row in rows)
 
-    def __getattr__(self, item):
+    def __getattr__(self, item: str):
         return getattr(self.writer, item)
 
 
-def writer(csvfile, dialect='excel', **fmtparams):
+def writer(csvfile: "io.TextIOBase", /, dialect='excel', **fmtparams):
     return _ProxyWriter(_basewriter(csvfile, dialect, **fmtparams))
 
 
 class DictWriter(_BaseDictWriter):
-    def __init__(self, f, fieldnames, restval="", extrasaction="raise",
-                 dialect="excel", *args, **kwds):
+    def __init__(self, f: "io.TextIOBase", fieldnames: "Sequence[str]", restval="",
+                 extrasaction="raise", dialect="excel", *args, **kwds):
         super().__init__(f, fieldnames, restval, extrasaction, dialect, *args, **kwds)
         self.writer = writer(f, dialect, **kwds)
